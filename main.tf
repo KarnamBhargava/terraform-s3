7beeb69a8993b46073ccda4b2b9d54af39deedc2
KarthikNarsapur <karthik.n@cloudthat.com>
2025-04-21 17:12:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "aws_s3_bucket" "my_bucket" {
 
 resource "aws_s3_bucket_versioning" "versioning" {
   count  = var.versioning ? 1 : 0
-  bucket = aws_s3_bucket.bucket.id
+  bucket = aws_s3_bucket.my_bucket.id
 
   versioning_configuration {
     status = "Enabled"
